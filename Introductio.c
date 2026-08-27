be387fe458afb327a917a46aa2aad2a6375e1fd6
ry727777 <ry727777@gmail.com>
2026-08-27 17:13:07 +0530

    Format Specifier

---
+++ Introductio.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+//	printf("My name is Rahul Yadav");
+//	printf("I am 26 years old.");
+
+	// Escape Sequence
+	printf("Name\tAge\tSection\n");
+	printf("Rahul\t26\tAK\n");
+	printf("Hi, \"Good Evening.\"");
+	
+	
+}
---
+++ formatSpecifier.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+	
+	// Format Specifier
+	printf("%d %d %d\n",26,45,67); // %d used to print Integer
+	printf("%.2f\n",45.567); // %f used to print decimal or float value
+	// %.2f print up to 2 decimal 
+	printf("%c",'A'); // %c is used to print char
+	
+	return 0;
+}
